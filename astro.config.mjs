a5e4eb136bc7317a86d0f998b2969cced31750a6
Gerardo Gallardo Rodríguez <gherardgr2@gmail.com>
2024-04-14 19:27:43 -0400

    added complete page

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://gerardoegr.github.io',
+  base: '/region-aysen-page',
+});
