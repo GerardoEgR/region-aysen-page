db85ca60dda9ac69816f244f04301d198ae5dea2
Gerardo Gallardo <gherardgr2@gmail.com>
2025-12-28 00:38:19 -0300

    feat(deploy): add workflow to deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,9 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import githubPages from "@astrojs/github-pages";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://gerardoegr.github.io',
-  base: '/region-aysen-page',
+  site: "https://gerardoegr.github.io",
+  base: "/region-aysen-page",
+  adapter: githubPages(),
 });
