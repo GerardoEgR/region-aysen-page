16cbcb8539591b0e96a7dc8a5ddbb7d8d2c741ea
Gerardo Gallardo <gherardgr2@gmail.com>
2025-12-28 00:55:54 -0300

    refactor(workflow): fix issues with the deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from "astro/config";
-import githubPages from "@astrojs/github-pages";
 
-// https://astro.build/config
 export default defineConfig({
-  site: "https://gerardoegr.github.io",
-  base: "/region-aysen-page",
-  adapter: githubPages(),
+  site: "https://tu-usuario.github.io",
+  base: "/region-aysen-page/",
+  output: "static",
 });
